8d47f5f16506bf5181b12db578e0db3326c89c1e
Lex <lex@deviantart.com>
2015-01-29 11:48:53 -0500

    implementation filled in by chris bolt

--- da-ebs-snapshot.py
+++ da-ebs-snapshot.py
@@ -1,86 +1,110 @@
+#!/usr/bin/env python
+
 import sys
 import os
-import boto
+import subprocess
+import boto.ec2
+import boto.utils
 import argparse
+import MySQLdb
 
 
-def parse_args:
+def parse_args():
+    parser = argparse.ArgumentParser(description='snapshot EBS volumes')
+
+    parser.add_argument('mountpoint', help='mount point of volume to snapshot')
+    parser.add_argument('-n', '--dry-run', action='store_true', help='help text')
+    parser.add_argument('--access-key-id')
+    parser.add_argument('--secret-access-key')
+    parser.add_argument('-r', '--region', default='us-east-1', help='EC2 region of EBS volume (default: %(default)s)')
+    parser.add_argument('--mysql-host')
+    parser.add_argument('--mysql-user')
+    parser.add_argument('--mysql-password')
+
+    args = parser.parse_args()
+    return args
+
+
+def find_dependencies(config):
     pass
 
 
-def find_dependencies:
-    pass
+def get_block_device(mountpoint):
+    mounts = {}
+    with open('/proc/mounts', 'r') as proc_mounts:
+        lines = proc_mounts.read().splitlines()
+    for line in lines:
+        columns = line.split(' ')
+        mounts[columns[1]] = columns[0]
+    return mounts[mountpoint]
 
 
-def connect_ec2:
-    pass
+def connect_ec2(config):
+    conn = boto.ec2.connect_to_region(config.region,
+                                      aws_access_key_id=config.access_key_id,
+                                      aws_secret_access_key=config.secret_access_key)
+    return conn
 
 
-def find_volume_id:
-    pass
+def get_volume_id(conn, block_device):
+    instance_id = boto.utils.get_instance_metadata()['instance-id']
+    volumes = conn.get_all_volumes(filters={
+                                            'attachment.instance-id': instance_id
+                                           })
+    for volume in volumes:
+        if volume.attach_data.device == block_device:
+            return volume.id
+        if volume.attach_data.device.replace('/dev/xv', '/dev/s'):
+            return volume.id
+    return None
 
 
-def lock_mysql:
-    pass
+def connect_mysql(config):
+    conn = MySQLdb.connect(host=config.mysql_host, user=config.mysql_user,
+                           passwd=config.mysql_password, db='')
+    return conn.cursor()
 
 
-def sync_fs:
-    pass
+def lock_mysql(mysql):
+    mysql.execute('FLUSH TABLES WITH READ LOCK')
 
 
-def freeze_fs:
-    pass
+def freeze_fs(mountpoint):
+    proc = subprocess.check_output(['/sbin/fsfreeze', '-f', mountpoint])
+    print 'fsfreeze -f returned: %s' % proc
 
 
-def snapshot:
-    pass
+def snapshot(conn, volume_id):
+    return conn.create_snapshot(volume_id)
 
 
-def unfreeze_fs:
-    pass
+def unfreeze_fs(mountpoint):
+    proc = subprocess.check_output(['/sbin/fsfreeze', '-u', mountpoint])
+    print 'fsfreeze -u returned: %s' % proc
 
 
-def unlock_mysql:
-    pass
+def unlock_mysql(mysql):
+    mysql.execute('UNLOCK TABLES')
 
 
-def declare_victory:
-    pass
+def declare_victory():
+    print "VICTORY IS MINE"
 
 
 def main():
-    steps = (parse_args,
-             find_dependencies,
-             connect_ec2,
-             find_volume_id,
-             lock_mysql,
-             sync_fs,
-             freeze_fs,
-             snapshot,
-             unfreeze_fs,
-             unlock_mysql,
-             declare_victory)
+    config = parse_args()
+    conn = connect_ec2(config)
 
-    config = None
-
-    try:
-        for step in steps:
-            result = step(config)
-
-            if config is None:
-                config = result
-    except Exception, e:
-        print >> sys.stderr, "Error on step %s: %s %s" % (step.func_name, type(e), e)
-
-        try:
-            for hook in config.cleanup_hooks:
-                hook()
-        except AttributeError:
-            pass
-        except Exception, e:
-            print >> sys.stderr, "Error in cleanup hook %s: %s %s" % (hook.func_name, type(e), e)
-            print >> sys.stderr, "MySQL may still be locked and/or fs may still be frozen; MANUAL CLEANUP NECESSARY!"
+    block_device = get_block_device(config.mountpoint)
+    volume_id = get_volume_id(conn, block_device)
+    mysql = connect_mysql(config)
+    lock_mysql(mysql)
+    freeze_fs(config.mountpoint)
+    print snapshot(conn, volume_id)
+    unfreeze_fs(config.mountpoint)
+    unlock_mysql(mysql)
+    declare_victory()
 
 
-if __name__ == __main__:
-    sys.exit(main(sys.argv))
+if __name__ == '__main__':
+    sys.exit(main())
